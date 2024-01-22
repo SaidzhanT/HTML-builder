658ac7994cd12e425c21ed9f88f1bbc2fb5d7d2d
SaidzhanT <saidzhanturdakhunov@gmail.com>
2024-01-22 21:05:21 +0700

    feat:complete 2 task out of 6

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+console.log('Введи желаемый текст или "exit" для выхода:');
+
+const fs = require('node:fs');
+const readline = require('node:readline');
+const inputOutput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const streamRecording = fs.createWriteStream('02-write-file.txt');
+inputOutput.on('line', (input) => {
+  if (input !== 'exit') {
+    streamRecording.write(input + '\n');
+    console.log('Введите еще текст или "exit" для выхода:');
+  } else {
+    console.log('До свидания!');
+    process.exit(0);
+  }
+});
+
